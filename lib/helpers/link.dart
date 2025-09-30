6e2aa00ee9030d700cf67fb237cb7a6dbac4e693
McKakTus <socol.maxim.it@gmail.com>
2025-09-30 16:57:40 +0300

    Remove firebase

--- lib/helpers/link.dart
+++ lib/helpers/link.dart
@@ -1,26 +1,7 @@
-import 'package:firebase_remote_config/firebase_remote_config.dart';
-
 class AppLinks {
   static String privacyPolicy =
       'https://www.privacypolicies.com/live/';
 
   static String terms =
       'https://app.websitepolicies.com/policies/view/';
-
-  static Future<void> updateFromRemoteConfig() async {
-    final remoteConfig = FirebaseRemoteConfig.instance;
-
-    await remoteConfig.setConfigSettings(
-      RemoteConfigSettings(
-        fetchTimeout: const Duration(seconds: 10),
-        minimumFetchInterval: const Duration(hours: 1),
-      ),
-    );
-
-    await remoteConfig.fetchAndActivate();
-
-    final newPrivacy = remoteConfig.getString('privacyPolicy');
-
-    if (newPrivacy.isNotEmpty) privacyPolicy = newPrivacy;
-  }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,15 +5,11 @@
 import FlutterMacOS
 import Foundation
 
-import firebase_core
-import firebase_remote_config
 import flutter_tts
 import shared_preferences_foundation
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FirebaseRemoteConfigPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
